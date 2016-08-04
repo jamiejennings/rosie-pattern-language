0e9aca4843a66d5a591ed2693b0f1ac808b24dc7
Jamie Jennings <jjennings@us.ibm.com>
2016-08-04 14:28:33 -0400

    FINALLY figured out how to use node ffi to move char * objects around!

--- src/c/librosie/new_ffi_test.js
+++ src/c/librosie/new_ffi_test.js
@@ -15,31 +15,15 @@ var debug = require("debug");
 var ffi = require("ffi");
 var ref = require('ref');
 var Struct = require('ref-struct');
+var ArrayType = require('ref-array')
 
 var DynamicLibrary = require('./node_modules/ffi/lib/dynamic_library')
   , ForeignFunction = require('./node_modules/ffi/lib/foreign_function')
 
-var libm = new ffi.Library("libm", { "ceil": [ "double", [ "double" ] ] });
-console.log(libm.ceil(3.5)); // 4
- 
-var current = new ffi.Library(null, { "atoi": [ "int32", [ "string" ] ] });
-console.log(current.atoi("01234")); // 1234 
-
-var TimeVal = Struct({
-  'tv_sec': 'long',
-  'tv_usec': 'long'
-});
-var TimeValPtr = ref.refType(TimeVal);
-
-var lib = new ffi.Library(null, { 'gettimeofday': [ 'int', [ TimeValPtr, "pointer" ] ]});
-var tv = new TimeVal();
-lib.gettimeofday(tv.ref(), null);
-console.log("Seconds since epoch: " + tv.tv_sec);
-
 
 var MyCString = Struct({
-  'len': 'uint32',
-  'ptr': 'string'
+    'len': 'uint32',
+    'ptr': ref.refType('CString')
 });
 
 var MyCStringArray = Struct({
@@ -57,14 +41,10 @@ var Rosie = new DynamicLibrary('librosie.so' || null, mode);
 
 var funcs = {'initialize': [ 'int', ['string']],
 	     'new_engine': [ MyCStringArray, [MyCStringPtr] ],
-	     'rosie_api': [ MyCStringArray, ['string', MyCStringPtr, MyCStringPtr] ],
-	     // 'testbyref': [ 'int', [MyCStringPtr] ],
-	     // 'testbyvalue': [ 'int', [MyCString] ],
-	     // 'testretstring': [ MyCString, [MyCStringPtr] ],
-	     // 'testretarray': [ MyCStringArray, [MyCString] ]
+	     'rosie_api': [ MyCStringArray, ['string', MyCStringPtr, MyCStringPtr] ]
 	    }
 
-var lib;
+var lib = new ffi.Library;
 Object.keys(funcs || {}).forEach(function (func) {
     debug('defining function', func)
 
@@ -91,85 +71,84 @@ Object.keys(funcs || {}).forEach(function (func) {
     }
   })
 
+
+var buf = ref.allocCString('hello world');
+buf[3] = 0
+console.log(buf.length, buf.toString());
+
 var str = new MyCString
-str.len = 7
-str.ptr = "Hello, world"
-console.log("str len=", str.len, " and ptr=", str.ptr)
+str.ptr = ref.allocCString("Hi!").ref()
+str.len = str.ptr.deref().length
+console.log("str.len =", str.len, " and str.ptr.length =", str.ptr.length)
+console.log("str len=", str.len, " and ptr=", str.ptr.deref().toString("utf8",0,str.len))
 
-// var retval = lib.testbyref(str.ref())
-// console.log("testbyref retval=", retval)
+console.log(buf.length, buf.toString());
+var xx = buf.ref()
+var x = xx.deref().reinterpret(Buffer.byteLength(buf.toString()))
+x = x.ref()
+console.log(buf.length, x.deref().toString().length, Buffer.byteLength(buf.toString()))
+console.log("x is:", x, " x.length=", x.length, " and ptr=", x.deref().toString("utf8", 0, buf.length))
+console.log("x.deref() is:", x.deref())
+x.deref()[1]=0
+console.log("x is:", x, " x.length=", x.length, " and ptr=", x.deref().toString("utf8", 0, buf.length))
+console.log("x.deref() is:", x.deref())
 
-// var retval = lib.testbyvalue(str)
-// console.log("testbyvalue retval=", retval)
-
-// var retval = lib.testretstring(str.ref())
-// console.log("testretstring retval=", retval)
-
-// var retval = lib.testretarray(str)
-// console.log("testretarray retval=", retval)
-
-// var n = retval.len
-// var p = retval.ptr
-// for (i=0; i<n; i++) {
-//     var cstr = ref.alloc(MyCString)
-//     cstr_ptr = ref.get(p, (i*ref.sizeof.pointer), MyCStringPtr)
-//     cstr = cstr_ptr.deref()
-//     console.log(i, "len=", cstr.len, "and ptr=", cstr.ptr.slice(0,cstr.len))
-// }
-
-function print_array(retval) {
-    var n = retval.n
-    var p = retval.ptr
-    for (var i=0; i<n; i++) {
-	var cstr = ref.alloc(MyCString)
-	var cstr_ptr = ref.get(p, (i*ref.sizeof.pointer), MyCStringPtr)
-	cstr = cstr_ptr.deref()
-	console.log("[",i, "] len =", cstr.len, "and ptr =", cstr.ptr.slice(0,cstr.len))
-    }
+function new_CString(str) {
+    var cstr = new MyCString
+    cstr.ptr = ref.allocCString(str)
+    cstr.len = str.length
+    return cstr
 }
 
-function extract_string(stringarray, index) {
+function extract_string_from_CString_ptr(cstr_ptr) {
+    var cstr = ref.alloc(MyCString)
+    cstr = cstr_ptr.deref()
+    var buf = cstr.ptr.reinterpret(cstr.len)
+    return buf.toString()
+}    
+
+function extract_string_from_array(stringarray, index) {
     var n = stringarray.n
     var p = stringarray.ptr
     if (index < n) {
-	var cstr = ref.alloc(MyCString)
 	var cstr_ptr = ref.get(p, (index*ref.sizeof.pointer), MyCStringPtr)
-	cstr = cstr_ptr.deref()
-	return cstr.ptr.slice(0,cstr.len)
+	var str = extract_string_from_CString_ptr(cstr_ptr)
+	return str
     }
     else return null
 }
 
+function print_array(retval) {
+    var n = retval.n
+    for (var i=0; i<n; i++) {
+	var str = extract_string_from_array(retval, i)
+	console.log("print_array: [", i, "] length =", str.length, " and value =", str)
+    }
+}
 
 console.log("About to initialize Rosie")
 var i = lib.initialize("/Users/jjennings/Work/Dev/rosie-pattern-language")
-console.log(i)
+console.log("Return value from initialize: ", i)
 
 var config = new MyCString
 var tbl = {'name': 'JS test engine', 'expression':'[:digit:]+', 'encode':false}
 var tmp = JSON.stringify(tbl)
-console.log(tmp)
-
-config.ptr = tmp
-config.len = tmp.length
-console.log(config.len, config.ptr.slice(0,config.len))
+config.ptr = ref.allocCString(tmp)
+config.len = Buffer.byteLength(tmp)
+var buf = config.ptr.reinterpret(config.len)
+console.log(config.ptr.length, config.len, buf.toString("utf8", 0, config.len))
 
 var i = ref.alloc(MyCStringArray)
 i = lib.new_engine(config.ref())
 console.log("Return value from new_engine is: ")
+var code = extract_string_from_array(i, 0)
+var eid = extract_string_from_array(i, 1)
 print_array(i)
-
-var ignored = new MyCString
-ignored.ptr = "ignored"
-ignored.len = ignored.ptr.len
-
-eid = extract_string(i, 1)
 console.log("Engine id is: ", eid)
 
-eid_CString = new MyCString
-eid_CString.ptr = eid.slice(0,10)
-eid_CString.len = 10
-console.log("Engine id as CString is: ", eid_CString.len, eid_CString.ptr)
+var ignored = new_CString("ignored")
+var eid_CString = new_CString(eid)
+console.log("Engine id as CString is: len =", eid_CString.len, "and ptr =", eid_CString.ptr.toString())
 
 i = lib.rosie_api("inspect_engine", eid_CString.ref(), ignored.ref())
 console.log("Return value from inspect_engine is: ")
