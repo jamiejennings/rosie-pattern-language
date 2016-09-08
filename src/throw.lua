bfaf79b5ceafb36f218db22847b580692ee432fd
Jamie Jennings <jjennings@us.ibm.com>
2016-09-08 17:27:39 -0400

    Renamed throw

--- src/throw.lua
+++ src/throw.lua
@@ -6,7 +6,7 @@
 
 local co = require "coroutine"
 
-function call(f, ...)
+function catch(f, ...)
    local c = co.create(f)
    local results = { co.resume(c, ...) }
    if results[1] then
@@ -20,19 +20,21 @@ end
 throw = co.yield
 
 if false then
-   call(table.print, {"a", "b"})
+   tprint = table.print or function(t) for k,v in pairs(t) do print(k,v); end; end
+      
+   catch(tprint, {"a", "b"})
    --> prints the table, no errors
-   print(pcall(call, table.print, "asd", {"a", "b"}))
+   print(pcall(catch, tprint, "asd", {"a", "b"}))
    --> error called, util.lua:106 bad argument #1 to 'pairs'
-   print(call(function() print("HELLO, WORLD!"); return 1,2,3; end))
+   print(catch(function() print("HELLO, WORLD!"); return 1,2,3; end))
    --> prints HELLO, WORLD!, and then 1  2  3
-   print(call(function() throw(5); print("HELLO, WORLD!"); return 1,2,3; end))
+   print(catch(function() throw(5); print("HELLO, WORLD!"); return 1,2,3; end))
    --> prints 5
-   print(nil==(call(function() throw(); print("HELLO, WORLD!"); return 1,2,3; end)))
+   print(nil==(catch(function() throw(); print("HELLO, WORLD!"); return 1,2,3; end)))
    --> prints true
    ok, msg = pcall(function() throw(4); end)
    assert(not ok)
    assert(msg=="attempt to yield from outside a coroutine")
-   k = call(pcall, function() throw(4); return 91921919; end)
+   k = catch(pcall, function() throw(4); return 91921919; end)
    assert(k==4)
 end
