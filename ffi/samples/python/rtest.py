50bf0781a5d604e4251fcd4b1dbe8a43f084ca30
Jamie Jennings <jjennings@us.ibm.com>
2016-10-04 12:35:11 -0400

    Added python example for calling librosie

---
+++ ffi/samples/python/rtest.py
@@ -0,0 +1,142 @@
+# coding: utf-8
+#  -*- Mode: Python; -*-                                              
+# 
+#  rtest.py     A crude sample program in Python that uses librosie
+# 
+#  © Copyright IBM Corporation 2016.
+#  LICENSE: MIT License (https://opensource.org/licenses/mit-license.html)
+#  AUTHOR: Jamie A. Jennings
+
+# Developed using MacOSX Python 2.7.10, cffi 1.7.0
+# easy_install --user cffi
+
+from cffi import FFI
+import json
+
+ffi = FFI()
+
+# The C code below was taken from librosie.h (and librosie_gen.h)
+ffi.cdef("""
+typedef uint8_t * byte_ptr;
+
+struct string {
+     uint32_t len;
+     byte_ptr ptr;
+};
+
+struct stringArray {
+     uint32_t n;
+     struct string **ptr;
+};
+
+struct string *new_string(char *msg, size_t len);
+struct stringArray *new_stringArray();
+void free_string(struct string s);
+void free_string_ptr(struct string *s);
+void free_stringArray(struct stringArray r);
+void free_stringArray_ptr(struct stringArray *r);
+
+void *initialize(struct string *rosie_home, struct stringArray *msgs);
+void finalize(void *L);
+
+struct stringArray clear_environment(void *L, struct string *optional_identifier);
+struct stringArray match(void *L, struct string *input_text, struct string *optional_start);
+struct stringArray set_match_exp_grep_TEMPORARY(void *L, struct string *pattern_exp);
+struct stringArray get_environment(void *L, struct string *optional_identifier);
+struct stringArray load_manifest(void *L, struct string *manifest_file);
+struct stringArray load_file(void *L, struct string *path);
+struct stringArray configure_engine(void *L, struct string *config_obj);
+struct stringArray load_string(void *L, struct string *input);
+struct stringArray info(void *L);
+struct stringArray inspect_engine(void *L);
+struct stringArray eval(void *L, struct string *input_text, struct string *start);
+struct stringArray eval_file(void *L, struct string *infilename, struct string *outfilename, struct string *errfilename, struct string *wholefileflag);
+struct stringArray match_file(void *L, struct string *infilename, struct string *outfilename, struct string *errfilename, struct string *wholefileflag);
+
+""")
+
+def to_cstr_ptr(py_string):
+    return Rosie.new_string(py_string, len(py_string))
+
+def from_cstr_ptr(cstr_ptr):
+    return ffi.buffer(cstr_ptr.ptr, cstr_ptr.len)[:]
+
+def strings_from_array(a):
+    lst = []
+    for i in range(0, a.n):
+        lst.append(from_cstr_ptr(a.ptr[i]))
+    return lst
+
+def printArray(a, caller_name):
+    print "Values returned from", caller_name
+    print " ", a.n, "values:"
+    lst = strings_from_array(a)
+    for i in range(len(lst)):
+        print " [", i, "] len =", a.ptr[i].len, " and  ptr =", lst[i]
+    return
+
+Rosie = ffi.dlopen("librosie.so")
+
+messages = Rosie.new_stringArray()
+engine = Rosie.initialize(to_cstr_ptr("/Users/jjennings/Work/Dev/public/rosie-pattern-language"), messages)
+printArray(messages, "initialize")
+Rosie.free_stringArray_ptr(messages)
+
+config_raw = "{\"expression\": \"[:digit:]+\", \"encode\": \"json\"}"
+config = to_cstr_ptr(config_raw)
+print("config (as cstr pointer): " + from_cstr_ptr(config))
+
+r = Rosie.configure_engine(engine, config)
+printArray(r, "configure_engine")
+retvals = strings_from_array(r)
+print retvals
+Rosie.free_stringArray(r)
+
+r = Rosie.inspect_engine(engine)
+printArray(r, "inspect_engine")
+retvals = strings_from_array(r)
+Rosie.free_stringArray(r)
+
+tbl = json.loads(retvals[1])
+print("Return from inspect_engine is: code = " + retvals[0] + ", tbl=" + str(tbl))
+
+print
+
+r = Rosie.load_manifest(engine, to_cstr_ptr("$sys/MANIFEST"))
+print(strings_from_array(r))
+Rosie.free_stringArray(r)
+
+print
+
+##
+## N.B. We can't use ffi.string (because it uses NULL to mark the end of a string, like C does)
+##
+
+start = to_cstr_ptr("3")        # 1-based indexing, so this starts matching at 3rd character
+
+foo = "1239999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999";
+foo_string = to_cstr_ptr(foo);
+r = Rosie.match(engine, foo_string, start); 
+printArray(r, "match");
+Rosie.free_stringArray(r)
+        
+foo = "1230000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+foo_string = to_cstr_ptr(foo);
+r = Rosie.match(engine, foo_string, ffi.NULL); # 'None' does not work here
+printArray(r, "match");
+Rosie.free_stringArray(r)
+
+config = to_cstr_ptr("{\"expression\": \"(basic.network_patterns)+\"}")
+r = Rosie.configure_engine(engine, config)
+printArray(r, "configure_engine")
+Rosie.free_stringArray(r)
+
+input_string = to_cstr_ptr("10.0.0.1 www.ibm.com foobar@example.com") 
+r = Rosie.match(engine, input_string, ffi.NULL)  # 'None' does not work here
+printArray(r, "match")
+Rosie.free_stringArray(r)
+
+
+
+Rosie.finalize(engine)
+
