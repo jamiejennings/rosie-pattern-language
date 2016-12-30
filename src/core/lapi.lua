bc4f7ba3557a212db89f8a62d32cafb65b1930ec
Jamie Jennings <jjennings@us.ibm.com>
2016-12-29 20:21:40 -0500

    In the middle of simplifying (ha) the way that rosie is bootstrapped

--- src/core/lapi.lua
+++ src/core/lapi.lua
@@ -56,7 +56,7 @@ end
 function lapi.new_engine(optional_cfg)
    optional_cfg = optional_cfg or {}
    if not optional_cfg.name then optional_cfg.name = "<anonymous>"; end
-   local en = engine(optional_cfg.name, common.new_env())
+   local en = engine.new(optional_cfg.name, common.new_env())
    local ok, msg = en:configure(optional_cfg)
    return en, ok, msg
 end
