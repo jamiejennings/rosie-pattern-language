27225ac0517724e5073679c728b5db4d63284667
Jamie Jennings <jjennings@us.ibm.com>
2017-02-07 16:13:47 -0500

    Default compiler on OS X is now clang.  Nodejs setup now sets CXX on OS X.

--- ffi/samples/nodejs/setup.sh
+++ ffi/samples/nodejs/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Creating script that sets ROSIE_HOME (and CXX for OS X)"
+echo -n >setvars
 
 REPORTED_PLATFORM=`(uname -o || uname -s) 2> /dev/null`
 if [ $REPORTED_PLATFORM = "Darwin" ]; then
@@ -15,17 +17,17 @@ echo "Making link to 'librosie.so'"
 ln -fs $LIB/librosie.so .
 
 if [ $PLATFORM = "macosx" ]; then
+    echo "export CXX=clang++" >>setvars
     echo "Creating dSYM version of librosie.so"
     dsymutil librosie.so
 fi
 
 if [ -z $ROSIE_HOME ]; then
-    echo "Creating script that sets ROSIE_HOME"
     ROSIE_HOME=`cd ../../.. && pwd`
-    echo "export ROSIE_HOME=$ROSIE_HOME" >setvars
-    echo "Use 'source setvars' to set ROSIE_HOME, then: node rtest.js"
+    echo "export ROSIE_HOME=$ROSIE_HOME" >>setvars
 else
     echo "ROSIE_HOME is already set to: $ROSIE_HOME"
-    echo "Run the sample program with: node rtest.js"
 fi
 
+echo "Do 'source setvars', then run sample program with: node rtest.js"
+echo "  (If nodejs prereqs are not installed, do: npm install debug ref-array ffi)"
