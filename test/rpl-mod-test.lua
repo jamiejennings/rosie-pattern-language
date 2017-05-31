e63ae6910fa77dd19a6a04d6647437d1954db387
Jamie Jennings <jjennings@us.ibm.com>
2017-05-31 12:05:24 -0400

    Wrote some basic module tests

--- test/rpl-mod-test.lua
+++ test/rpl-mod-test.lua
@@ -52,8 +52,64 @@ check(not ok)
 check(type(m)=="table")
 check(#m==1)
 
+ok, _, msgs = e:load("thisfiledoesnotexist!")
+check(not ok)
+check(type(msgs)=="table")
+
+ok, _, msgs = e:load("import mod2")
+check(ok)
+p = e:lookup("mod2.x")
+check(not p)
+p = e:lookup("mod2.y")
+check(p)
+
+ok, _, msgs = e:load("import mod2 as foobar")
+check(ok)
+p = e:lookup("foobar.x")
+check(not p)
+p = e:lookup("foobar.y")
+check(p)
+
+p = e:lookup("x")
+check(not p)
+p = e:lookup("y")
+check(not p)
+
+ok, pkgname, msgs = e:load("import mod2 as .")
+check(ok)
+check(not pkgname)
+p = e:lookup("x")
+check(not p)
+p = e:lookup("y")
+check(p)
+
+ok, pkgname, msgs = e:load("import mod3")
+check(ok)
+check(not pkgname)
+p = e:lookup("mod3")
+check(not p)
+p = e:lookup("name_is_unexpectedly_different_from_file_name.y")
+check(p)
+
+ok, m, left, t0 = e:match("name_is_unexpectedly_different_from_file_name.y", "world")
+check(ok)
+check(m)
+table.print(m, false)
+check(m.type=="name_is_unexpectedly_different_from_file_name.y")
+check(left==0)
+p = e:lookup("name_is_unexpectedly_different_from_file_name.x")
+check(not p)
 
 
+ok, pkgname, msgs = e:load("import mod4")
+check(not ok)
+check(not pkgname)
+check(type(msgs)=="table")
+
+ok, pkgname, msgs = e:load("package foo")
+check(ok)
+check(pkgname=="foo")
+check(type(msgs)=="table")
 
 
 
