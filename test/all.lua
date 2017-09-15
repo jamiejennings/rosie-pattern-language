41c8e89af47c443e6488f72d877262e0d6a08235
Jamie Jennings <jjennings@us.ibm.com>
2017-09-15 12:38:20 -0400

    Added a very basic utf8 test.  Will add more later.

--- test/all.lua
+++ test/all.lua
@@ -23,7 +23,6 @@ test = import("test")
 json = require "cjson"
 
 test.dofile(ROSIE_HOME .. "/test/lib-test.lua")
-print("SKIPPING API TEST")--test.dofile(ROSIE_HOME .. "/test/api-test.lua")
 test.dofile(ROSIE_HOME .. "/test/rpl-core-test.lua")
 test.dofile(ROSIE_HOME .. "/test/rpl-mod-test.lua")
 test.dofile(ROSIE_HOME .. "/test/rpl-appl-test.lua")
@@ -33,7 +32,19 @@ test.dofile(ROSIE_HOME .. "/test/trace-test.lua")
 test.dofile(ROSIE_HOME .. "/test/cli-test.lua")
 test.dofile(ROSIE_HOME .. "/test/repl-test.lua")
 
+test.dofile(ROSIE_HOME .. "/test/utf8-test.lua")
+
 passed = test.print_grand_total()
+
+
+print("\nTESTING TODO LIST:")
+print("- macros")
+print("- input data with nulls")
+print("- new api")
+print("- more repl tests")
+print("- more trace tests")
+print()
+
 
 -- When running Rosie interactively (i.e. development mode), do not exit.  Otherwise, these tests
 -- were launched from the command line, so we should exit with an informative status.
