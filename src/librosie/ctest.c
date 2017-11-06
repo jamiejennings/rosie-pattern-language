d34666a43f303adaf7e20aa517904d6b0897938f
Jamie Jennings <jjennings@us.ibm.com>
2017-11-06 14:58:02 -0500

    Writing a very basic driver in C.  Working, but not pretty.  Makefile builds it as "ctest".

--- src/librosie/ctest.c
+++ src/librosie/ctest.c
@@ -7,10 +7,11 @@
 /*  AUTHOR: Jamie A. Jennings                                                */
 
 #include <dlfcn.h>
+#include <string.h>
 
-#include "librosie.h"
+#include "ctest.h"
 
-#define LIBROSIE_PATH "librosie.so"
+#define LIBROSIE "librosie.so"
 void *librosie;
 
 
@@ -37,13 +38,131 @@ void *librosie;
 static int init(const char *librosie_path){
   librosie = dlopen(librosie_path, RTLD_NOW);
   if (librosie == NULL) {
-    LOG("*** dlopen of librosie returned NULL\n");
+    LOGf("failed to dlopen %s\n", librosie_path);
     return FALSE;
   }
+  LOG("opened librosie\n");
   return TRUE;
 }
 
+#define bind_function(localname, libname) {	\
+  localname = dlsym(lib, libname);		\
+  if (localname == NULL) { \
+    msg = dlerror(); \
+    if (msg == NULL) { msg = "no error reported"; }	   \
+    LOGf("failed to bind %s, err is: %s\n", libname, msg); \
+    goto fail; \
+  } \
+  else { \
+  LOGf("bound %s\n", libname); \
+  }} while (0);		       \
+
+static int bind(void *lib){
+  char *msg = NULL;
+  fp_rosie_new = dlsym(lib, "rosie_new");
+  if (fp_rosie_new == NULL) {
+    msg = dlerror(); if (msg == NULL) msg = "no error reported";
+    LOGf("failed to bind %s, err is: %s\n", "rosie_new", msg);
+    goto fail;
+  }
+
+  bind_function(fp_rosie_new, "rosie_new");
+  bind_function(fp_rosie_finalize, "rosie_finalize");
+
+  bind_function(fp_rosie_new_string, "rosie_new_string");
+  bind_function(fp_rosie_free_string, "rosie_free_string");
+  bind_function(fp_rosie_new_string_ptr, "rosie_new_string_ptr");
+  bind_function(fp_rosie_free_string_ptr, "rosie_free_string_ptr");
+
+  bind_function(fp_rosie_setlibpath, "rosie_setlibpath_engine");
+  bind_function(fp_rosie_set_alloc_limit, "rosie_set_alloc_limit");
+  bind_function(fp_rosie_config, "rosie_config");
+
+  bind_function(fp_rosie_compile, "rosie_compile");
+  bind_function(fp_rosie_free_rplx, "rosie_free_rplx");
+  bind_function(fp_rosie_match, "rosie_match");
+  bind_function(fp_rosie_matchfile, "rosie_matchfile");
+  bind_function(fp_rosie_trace, "rosie_trace");
+
+  bind_function(fp_rosie_load, "rosie_load");
+  bind_function(fp_rosie_import, "rosie_import");
+
+  LOG("Bound the librosie functions\n");
+  return TRUE;
+
+ fail:
+  LOG("Failed to bind librosie functions\n");
+  return FALSE;
+}
+
+
+
 int main() {
-  init(LIBROSIE_PATH);
-  LOG("opened librosie\n");
+  init(LIBROSIE);
+
+  bind(librosie);
+
+  str errors;
+  void *engine = (*fp_rosie_new)(&errors);
+  if (engine == NULL) {
+    LOG("rosie_new failed\n");
+    return FALSE;
+  }
+  LOG("obtained rosie matching engine\n");
+
+  int err;
+  int ok;
+  str pkgname, as;
+  pkgname = (*fp_rosie_new_string)("all", 3);
+  errors = (*fp_rosie_new_string)("", 0);
+  printf("pkgname = %s; as = %s; errors = %s\n", pkgname.ptr, as.ptr, errors.ptr);
+  LOG("allocated strs\n");
+  err = (*fp_rosie_import)(engine, &ok, &pkgname, NULL, &errors);
+  if (err) {
+    LOG("rosie call failed: import library \"all\"\n");
+    goto quit;
+  }
+  if (!ok) {
+    printf("failed to import the \"all\" library with error code %d\n", ok);
+    goto quit;
+  }
+
+#define STR(literal) (*fp_rosie_new_string)((literal), strlen((literal)));
+
+  int pat;
+  str expression = STR("all.things");
+  err = (*fp_rosie_compile)(engine, &expression, &pat, &errors);
+  if (err) {
+    LOG("rosie call failed: compile expression\n");
+    goto quit;
+  }
+  if (!pat) {
+    printf("failed to compile expression; error returned was:\n");
+    if (errors.ptr != NULL) {
+      printf("%s\n", errors.ptr);
+    }
+    else {
+      printf("no error message given\n");
+    }
+    goto quit;
+  }
+
+  str input = STR("1234");
+  match m;
+  err = (*fp_rosie_match)(engine, pat, 1, "json", &input, &m);
+  if (err) {
+    LOG("rosie call failed: match");
+    goto quit;
+  }
+  if (!m.data.ptr) {
+    printf("match failed\n");
+  }
+  else {
+    printf("match data is: %.*s\n", m.data.len, m.data.ptr);
+  }
+    
+
+ quit:
+  (*fp_rosie_finalize)(engine);
+  
 }
---
+++ src/librosie/ctest.h
@@ -0,0 +1,81 @@
+/*  -*- Mode: C/l; -*-                                                       */
+/*                                                                           */
+/*  ctest.h                                                                  */
+/*                                                                           */
+/*  © Copyright IBM Corporation 2017.                                        */
+/*  LICENSE: MIT License (https://opensource.org/licenses/mit-license.html)  */
+/*  AUTHOR: Jamie A. Jennings                                                */
+
+#define INITIAL_RPLX_SLOTS 32
+#define INITIAL_ALLOC_LIMIT_MB 0
+#define MIN_ALLOC_LIMIT_MB 10
+
+#define TRUE 1
+#define FALSE 0
+
+#define MAX_ENCODER_NAME_LENGTH 64 /* arbitrary limit to avoid runaway strcmp */
+
+#define SUCCESS 0
+#define ERR_OUT_OF_MEMORY -2
+#define ERR_SYSCALL_FAILED -3
+#define ERR_ENGINE_CALL_FAILED -4
+
+/* These codes are returned in the length field of an str whose ptr is NULL */
+#define ERR_NO_MATCH 0
+#define ERR_NO_PATTERN 1
+#define ERR_NO_TRACESTYLE 2	/* same as no encoder */
+#define ERR_NO_FILE 3		/* no such file or directory */
+
+#include <stdint.h>
+#include <sys/param.h>		/* MAXPATHLEN */
+#include "../../submodules/rosie-lpeg/src/rpeg.h"
+
+typedef struct rosie_string str;
+
+typedef struct rosie_matchresult {
+     str data;
+     int leftover;
+     int abend;
+     int ttotal;
+     int tmatch;
+} match;
+
+void *(*fp_rosie_new)();
+str   (*fp_rosie_new_string)();
+void  (*fp_rosie_free_string)();
+str   (*fp_rosie_new_string_ptr)();
+void  (*fp_rosie_free_string_ptr)();
+void  (*fp_rosie_finalize)();
+int   (*fp_rosie_setlibpath)();
+int   (*fp_rosie_set_alloc_limit)();
+int   (*fp_rosie_config)();
+int   (*fp_rosie_compile)();
+int   (*fp_rosie_free_rplx)();
+int   (*fp_rosie_match)();
+int   (*fp_rosie_matchfile)();
+int   (*fp_rosie_trace)();
+int   (*fp_rosie_load)();
+int   (*fp_rosie_import)();
+
+/* REFERENCE:
+str rosie_new_string(byte_ptr msg, size_t len);
+str *rosie_new_string_ptr(byte_ptr msg, size_t len);
+void rosie_free_string(str s);
+void rosie_free_string_ptr(str *s);
+
+void *rosie_new(str *errors);
+void rosie_finalize(void *L);
+int rosie_setlibpath(void *L, char *newpath);
+int rosie_set_alloc_limit(void *L, int newlimit);
+int rosie_config(void *L, str *retvals);
+int rosie_compile(void *L, str *expression, int *pat, str *errors);
+int rosie_free_rplx(void *L, int pat);
+int rosie_match(void *L, int pat, int start, char *encoder, str *input, match *match);
+int rosie_matchfile(void *L, int pat, char *encoder, int wholefileflag,
+		    char *infilename, char *outfilename, char *errfilename,
+		    int *cin, int *cout, int *cerr,
+		    str *err);
+int rosie_trace(void *L, int pat, int start, char *trace_style, str *input, int *matched, str *trace);
+int rosie_load(void *L, int *ok, str *src, str *pkgname, str *errors);
+int rosie_import(void *L, int *ok, str *pkgname, str *as, str *errors);
+*/
