5b79958d5d4364f4a41ced0b120068571c71e17b
Jamie Jennings <jamiejennings@users.noreply.github.com>
2018-01-11 14:15:15 -0500

    Re-added go client to this branch

---
+++ src/librosie/go/setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+echo "Creating script that sets GOPATH and ROSIE_HOME"
+echo "export GOPATH=`pwd`" >setvars
+
+LIB=`cd .. && pwd`
+LUALIB=`cd ../../../submodules/lua/include && pwd`
+RPEGLIB=`cd ../../../submodules/rosie-lpeg/src && pwd`
+
+echo "Creating 'include' directory in 'src/rtest' and symlinks to librosie source"
+mkdir -p src/rosie/include
+ln -fs $LIB/librosie.h src/rosie/include/
+ln -fs $RPEGLIB/rpeg.h src/rosie/include/
+ln -fs $RPEGLIB/rbuf.h src/rosie/include/
+ln -fs $LUALIB/luaxlib.h src/rosie/include/
+
+echo "Linking librosie.a from librosie directory"
+ln -fs $LIB/librosie.a src/rosie/librosie.a
+
+if [ -z $ROSIE_HOME ]; then
+    ROSIE_HOME=`cd ../../.. && pwd`
+    echo "ROSIE_HOME not set.  Assuming rosie installation is $ROSIE_HOME"
+else
+    echo "ROSIE_HOME is already set to: $ROSIE_HOME"
+fi
+if [ ! -d rosie ]; then
+    echo "Creating link 'rosie' to rosie installation directory"
+    ln -fs $ROSIE_HOME rosie
+else
+    echo "Link 'rosie' to rosie installation directory already exists"
+fi
+
+
+echo "--------------------------------------------------------"
+echo "Source the file 'setvars' to set GOPATH and ROSIE_HOME:"
+echo '. setvars'
+echo "go build rtest"
+echo "./rtest"
+
+
+
+
+
+
---
+++ src/librosie/go/src/rtest/rtest.go
@@ -0,0 +1,68 @@
+//  -*- Mode: Go; -*-                                              
+// 
+//  rtest.go    Sample driver for librosie in go
+// 
+//  © Copyright IBM Corporation 2016, 2017, 2018.
+//  LICENSE: MIT License (https://opensource.org/licenses/mit-license.html)
+//  AUTHOR: Jamie A. Jennings
+
+
+package main
+
+import "rosie"
+
+import "fmt"
+import "os"
+import "runtime"
+
+func main() {
+
+//	runtime.LockOSThread()
+	
+	fmt.Printf("Initializing Rosie... ")
+	
+	engine, err := rosie.New("hi")
+	if engine == nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+	fmt.Printf("Engine is %v\n", engine)
+	engine, err = rosie.New("bye")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+	fmt.Printf("And another engine: %v\n", engine)
+	runtime.GC()
+	runtime.GC()
+	fmt.Printf("Engine is %v\n", engine)
+
+	var cfg rosie.Configuration
+ 	err = engine.Config(&cfg)
+	if err == nil {
+		for _, entry := range cfg {
+			fmt.Printf("%s = %s (%s)\n", entry["name"], entry["value"], entry["desc"])
+		}
+ 	} else {
+ 		fmt.Printf("Return value from config was: %v\n", err)
+ 		os.Exit(-1)
+ 	}
+
+	for i:=0; i<100; i++ {
+
+		runtime.GC()
+	
+		exp := "[:digit:]+"
+		pat, err := engine.Compile(exp)
+
+		if err != nil {
+			fmt.Println(pat, err)
+			os.Exit(-1)
+		} else {
+			fmt.Println("Successfully compiled pattern", pat)
+		}
+	}
+
+	fmt.Printf("Exiting...\n");
+
+}
